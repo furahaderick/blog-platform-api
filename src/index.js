2907402a2eddd36bcf64734c1c39a590b933307c
furahaderick <yvesderricks@gmail.com>
2024-09-14 15:28:08 +0200

    Use user router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 
 import startDB from "./configs/database.config.js";
+import userRouter from "./routers/user.router.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 startDB();
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
 
 // test route
 app.get("/api/test", (req, res) => {
